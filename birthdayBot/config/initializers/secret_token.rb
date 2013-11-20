13e33c2ecf897595418cb6377951e7d5b409291f
alex-2539 <alejandrogapan@yahoo.com>
2013-11-20 15:20:32 -0800

    Rails Setup

---
+++ birthdayBot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BirthdayBot::Application.config.secret_token = 'ec6f10c2fa6df37e6c2b70eca89e5ed2e0aca90f48be51787773a180bcdc9429766b89391bfad35464a518177a7608254cd76e90818bb1f85f79fcb16cf0b766'

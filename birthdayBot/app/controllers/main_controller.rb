fde76a52c4258b1dbc9d2743c0991522580645d3
alex-2539 <alejandrogapan@yahoo.com>
2013-11-21 10:14:48 -0800

    Changes

--- birthdayBot/app/controllers/main_controller.rb
+++ birthdayBot/app/controllers/main_controller.rb
@@ -1,5 +1,14 @@
 class MainController < ApplicationController
 
+  def index
+    unless current_user
+      redirect_to login_path
+    end
+  end
+
+  def login
+
+  end
 
 
 end
--- birthdayBot/config/routes.rb
+++ birthdayBot/config/routes.rb
@@ -1,7 +1,11 @@
 BirthdayBot::Application.routes.draw do
+
+  root to: "main#index"
+
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: 'sessions#create'
   match 'signout', to: 'sessions#destroy', as: 'signout'
+  match 'login', to: 'main#login'
 
-  root to: "main#login"
+
 end

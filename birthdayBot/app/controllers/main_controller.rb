b9b9959ebae7e8387002088b0ed057b337316d84
SkylerBenge <scbenge@hotmail.com>
2013-11-21 18:11:39 -0800

    seeds

--- birthdayBot/app/controllers/main_controller.rb
+++ birthdayBot/app/controllers/main_controller.rb
@@ -1,15 +1,17 @@
 class MainController < ApplicationController
-
-
   def new
-    unless current_user
-      redirect_to login_path
-    end
+
   end
 
   def post
     @post = current_user.facebook.put_wall_post(params[:message])
     redirect_to root_path
-  end
 
+    @post.answers.push(Quote.all.sample)
+
+    response = {post: @post,
+                quote: @post.quotes.last}
+
+    render json: response
+  end
 end
